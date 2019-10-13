999808e07e54a9f3e1d1d1e740f58b30c6556bab
L04DB4L4NC3R <angadsharma1016@gmail.com>
2019-10-13 23:15:31 +0530

    deployment script

--- scripts/telegram.sh
+++ scripts/telegram.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-GIT_SHA=1
+GIT_SHA=$(git rev-parse --short ${BUILD_SHA})
+CHAT_ID="-332180086"
 
 if [[ -e build/app/outputs/apk/release/app-release.apk  ]]
 
 then
 
-	mv build/app/outputs/apk/release/app-release.apk build/app/outputs/apk/release/hades_${GIT_SHA}.apk
+	curl -F chat_id=$CHAT_ID -F document=@"build/app/outputs/apk/release/hades_${GIT_SHA}.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 
-	curl -F chat_id="900025646" -F document=@"build/app/outputs/apk/release/hades_${GIT_SHA}.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
-
+	curl -F chat_id=$CHAT_ID -F text="Build succeeded for ${GIT_SHA}!" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 else
 
-	curl -F chat_id="900025646" -F text="Build failed!" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
+	curl -F chat_id=$CHAT_ID -F text="Build failed!" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 
 fi
